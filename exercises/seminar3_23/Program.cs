8446eefc37ad07a83c09907f93e9c3661b403c24
DENIS <hindoo@mail.ru>
2022-11-06 23:33:31 +0300

    third exercise

---
+++ exercises/seminar3_23/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void PrintlistOfCubes (int inputDigit)
+{
+   if (inputDigit == 0)
+   {
+    Console.WriteLine(0); 
+   }
+   else if (inputDigit > 0)
+   {
+    for (int i = 1; i <= inputDigit; i++)
+    {
+        Console.WriteLine(i*i*i);
+    }
+   }
+   else if (inputDigit < 0)
+   {
+    for (int i = -1; i >= inputDigit; i--)
+    {
+        Console.WriteLine(i*i*i);
+    }
+   }
+    
+}
+
+Console.WriteLine("Input number to output iist of cubes: ");
+string tmp = Console.ReadLine();
+bool check = int.TryParse(tmp, out int number);
+Console.WriteLine("");
+
+if (check)
+{
+    PrintlistOfCubes (number);
+}
+else
+{
+    Console.WriteLine("Wrong Input");
+}
